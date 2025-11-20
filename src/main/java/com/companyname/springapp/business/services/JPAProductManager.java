57193a96457ca7c6d54889ccbf565f4e3581a5b7
GeroniMolero <gmolrod892@g.educaand.es>
2025-11-20 13:38:39 +0100

    Terminado

--- src/main/java/com/companyname/springapp/business/services/JPAProductManager.java
+++ src/main/java/com/companyname/springapp/business/services/JPAProductManager.java
@@ -2,29 +2,33 @@ package com.companyname.springapp.business.services;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.companyname.springapp.business.entities.Product;
+import com.companyname.springapp.business.repositories.ProductRepository;
 
-public class SimpleProductManager implements ProductManager {
+@Service
+public class JPAProductManager implements ProductManager {
 
-    private List<Product> products;
+    @Autowired
+    private ProductRepository productRepository;
 
+    @Override
     public List<Product> getProducts() {
-        return products; 
+        return (List<Product>) productRepository.findAll();
     }
 
+    @Override
     public void increasePrice(int percentage) {
+        List<Product> products = (List<Product>) productRepository.findAll();
         if (products != null) {
             for (Product product : products) {
                 double newPrice = product.getPrice().doubleValue() * 
                                     (100 + percentage)/100;
                 product.setPrice(newPrice);
+                productRepository.save(product);
             }
         }  
-    }
-	
-    public void setProducts(List<Product> products) {
-        this.products = products;
     }
 }
